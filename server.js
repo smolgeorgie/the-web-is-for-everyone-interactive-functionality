e207fed46705721630c052553f4b0e37f65a21fc
smolgeorgie <ilona.van.oosbree@hva.nl>
2024-03-22 10:49:30 +0100

    things

--- server.js
+++ server.js
@@ -36,3 +36,6 @@ app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+// Scroll from Calculator to SDGs
+
